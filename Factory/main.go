e6b51664ebc0e9c9be8b0c971c393568a12f8703
Julio Paredes <ing.julio.code@gmail.com>
2023-07-06 18:59:37 -0600

    implementing factory pattern

---
+++ Factory/main.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+
+//crear interface o superclass
+type IProduct interface {
+	setStock(stock int)
+	getStock() int
+	setName(name string)
+	getName() string
+}
+
+type Computer struct {
+	name  string
+	stock int
+}
+
+func (c *Computer) setStock(stock int) {
+	c.stock = stock
+}
+
+func (c *Computer) setName(name string) {
+	c.name = name
+}
+
+func (c *Computer) getName() string {
+	return c.name
+}
+
+func (c *Computer) getStock() int {
+	return c.stock
+}
+
+type Laptop struct {
+	Computer
+}
+
+func newLaptop() IProduct {
+	return &Laptop{
+		Computer{
+			name:  "Laptop Computer",
+			stock: 25,
+		},
+	}
+}
+
+type Desktop struct {
+	Computer
+}
+
+func newDesktop() IProduct {
+	return &Desktop{
+		Computer{
+			name:  "Desktop Computer",
+			stock: 35,
+		},
+	}
+}
+
+func GetComputerFactory(computerType string) (IProduct, error) {
+	if computerType == "Laptop" {
+		return newLaptop(), nil
+	}
+
+	if computerType == "Desktop" {
+		return newDesktop(), nil
+	}
+
+	return nil, fmt.Errorf("Invalid Computer Type")
+}
+
+func printNameAndStock(p IProduct) {
+	fmt.Printf("Product name: %s, with stock %d\n", p.getName(), p.getStock())
+}
+
+func main() {
+	laptop, _ := GetComputerFactory("Laptop")
+	desktop, _ := GetComputerFactory("Desktop")
+
+	printNameAndStock(laptop)
+	printNameAndStock(desktop)
+}
